d7869d70134fed7bec5706c4bf93d4cef7f701da
tuxedo21 <ctps.21@comunidad.unam.mx>
2017-08-06 18:49:02 -0500

    Did BuildOrder! hard, and i might need to brush up on streams

--- src/CtCi/TreesAndGraphs/BuildOrder.java
+++ src/CtCi/TreesAndGraphs/BuildOrder.java
@@ -1,7 +1,82 @@
 package CtCi.TreesAndGraphs;
 
+import javax.print.DocFlavor;
+import java.util.*;
+
 /**
  * Created by tuxedo21 on 4/05/17.
  */
 public class BuildOrder {
+
+    public static Character[] getBuildOrder(char[] proj, ArrayList<char[]> depen){
+        Stack<Character> buildOrder = new Stack<>();
+        Character [] tF =  trueFathers(proj,depen);
+        ArrayList<char[]> garbage = new ArrayList<>();
+
+        for (Character e: tF){
+            buildOrder.push(e);
+        }
+
+            while (!depen.isEmpty()){
+
+//            //garbage.stream().filter(depen::contains).forEach(depen::remove);
+                for (char[] g: garbage){
+                    if (depen.contains(g))
+                        depen.remove(g);
+                } garbage.clear();
+
+                for (char[] e: depen) {
+                    if (buildOrder.contains(e[0])) {
+                        if (!buildOrder.contains(e[1]))
+                            buildOrder.push(e[1]);
+                        garbage.add(e);
+                    }
+                }
+            }
+
+        return buildOrder.toArray(new Character[buildOrder.size()]);
+
+    }
+
+    public static Character[] trueFathers(char[] proj, ArrayList<char[]> depen){
+
+        HashSet<Character> sons = getSons(depen);
+        HashSet<Character> hProj = gethProj(proj);
+        hProj.removeAll(sons);
+
+
+        return  hProj.toArray(new Character[hProj.size()]);
+
+    }
+
+    private static HashSet<Character> gethProj(char[] proj) {
+        HashSet<Character> sons = new HashSet<>();
+        for (char d: proj)
+            sons.add(d);
+        return sons;
+    }
+
+    public static HashSet<Character> getSons(ArrayList<char[]> depen){
+        HashSet<Character> sons = new HashSet<>();
+        for (char[] d: depen)
+            sons.add(d[1]);
+        return sons;
+
+    }
+
+    public static void main(String[] args) {
+        char [] projects = {'a','b','c','d','e','f'};
+        ArrayList<char[]> depen = new ArrayList<>();
+        depen.add(new char[]{'a', 'd'});
+        depen.add(new char[]{'f', 'b'});
+        depen.add(new char[]{'b', 'd'});
+        depen.add(new char[]{'f', 'a'});
+        depen.add(new char[]{'d', 'c'});
+
+        System.out.println(Arrays.toString(projects));
+        for (char[] d: depen)
+            System.out.println( Arrays.toString(d));
+
+        System.out.println("\n" + Arrays.toString(getBuildOrder(projects,depen)));
+    }
 }
